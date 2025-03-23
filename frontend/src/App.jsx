15537a50ff682c68db21906584acb48d5922b0a5
AshanDilakshana <167285793+AshanDilakshana@users.noreply.github.com>
2025-03-23 06:08:02 +0530

    add adminlogging

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 
 import Header from './components/header/header';
 import WorkOder from './components/workoderManager/workOders';
 import AdminProfile from './components/workoderManager/adminProfile';
+import AdminLoging from './components/Loging/loging';
 
 function App() {
   return (
     <div className="App">
-       
       <Router>
-      <ToastContainer position="top-right" autoClose={5000} />
-        <Header />
+       {/*  <Header />*/}
         <Routes>
           {/* Correctly define the Route with a valid path */}
           <Route path="/work-order-details" element={<WorkOder />} />
           <Route path="/sidebar" element={<AdminProfile/>} />
-
+          <Route path="/AdminLoging"element={<AdminLoging />} />
 
         </Routes>
       </Router>
