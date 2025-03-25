f7f35ada25786c27b40e198a2d8e8cb8d9b9b095
AshanDilakshana <167285793+AshanDilakshana@users.noreply.github.com>
2025-03-25 20:33:12 +0530

    update app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,17 +5,19 @@ import Header from './components/header/header';
 import WorkOder from './components/workoderManager/workOders';
 import AdminProfile from './components/workoderManager/adminProfile';
 import AdminLoging from './components/Loging/loging';
+import HOME from './components/Home/homePage';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Header />
+       {/* <Header /> */}
         <Routes>
-          {/* Correctly define the Route with a valid path */}
+          <Route path="/"element={<HOME />} />
           <Route path="/work-order-details" element={<WorkOder />} />
           <Route path="/sidebar" element={<AdminProfile/>} />
           <Route path="/AdminLoging"element={<AdminLoging />} />
+          
 
         </Routes>
       </Router>
